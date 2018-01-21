6defe03a26cf3a47d3a1004ab707211209a79db0
nextperiment <c7v6dh@gmail.com>
2018-01-21 21:02:15 +0100

    Fegyver osztály kiegészítése boolean _nyilvesszo mezővel, contructor létrehozása.

--- src/tormatargonca/entity/Fegyver.java
+++ src/tormatargonca/entity/Fegyver.java
@@ -5,6 +5,15 @@ public class Fegyver {
     private int _id;
     private String _nev;
     private String _leiras;
+    
+    private boolean _nyilvesszo;
+
+    public Fegyver(int _id, String _nev, String _leiras, boolean _nyilvesszo) {
+        this._id = _id;
+        this._nev = _nev;
+        this._leiras = _leiras;
+        this._nyilvesszo = _nyilvesszo;
+    }
 
     public int getId() {
         return _id;
