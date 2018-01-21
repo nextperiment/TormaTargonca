601c62334e4735066978f811326af721c1409858
nextperiment <c7v6dh@gmail.com>
2018-01-21 21:00:30 +0100

    Ellenség osztály kiegészítése _fegyverId mezővel, contructor létrehozása.

--- src/tormatargonca/entity/Ellenseg.java
+++ src/tormatargonca/entity/Ellenseg.java
@@ -29,12 +29,30 @@ public class Ellenseg {
     
     private int _buntetesSebzesErtek;
     // egy lény ha legyőzk vagy kincset ad vagy fegyvert (ha nem kincset, akkor fegyvert)
-    private boolean _kincsetAd;
+    //private boolean _kincsetAd;
     // 
     private Kincs _kincs;
     private Fegyver _fegyver;
     
+    private int _fegyverId;
     
+    public Ellenseg(int _id, boolean _foellenseg, String _nev, int _eletpont, int _tamadoertek, int _sebzes, int _tapasztalatipont, boolean _vanTavsebzes, TavsebzesTipusEnum _tavsebzesTipus, int _tavsebzesSebzesErtek, BuntetesTipusEnum _buntetesTipus, int _buntetesSebzesErtek, int _fegyverId) {
+        this._id = _id;
+        this._foellenseg = _foellenseg;
+        this._nev = _nev;
+        this._eletpont = _eletpont;
+        this._tamadoertek = _tamadoertek;
+        this._sebzes = _sebzes;
+        this._tapasztalatipont = _tapasztalatipont;
+        this._vanTavsebzes = _vanTavsebzes;
+        this._tavsebzesTipus = _tavsebzesTipus;
+        this._tavsebzesSebzesErtek = _tavsebzesSebzesErtek;
+        this._buntetesTipus = _buntetesTipus;
+        this._buntetesSebzesErtek = _buntetesSebzesErtek;
+        //this._kincsetAd = _kincsetAd;
+        //this._kincs = _kincs;
+        this._fegyverId = _fegyverId;
+    }
     
     
 
